c94c1f4e33c4356dc271837b407cb37275e4b8b9
addnewuser <addnewuser@yandex.com>
2022-11-05 10:09:42 +0300

    Add 1. Two Sum problem solution.

---
+++ src/main/java/com/addnewuser/solutions/twoSum/Solution.java
@@ -0,0 +1,31 @@
+/*
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+You can return the answer in any order.
+ */
+
+package com.addnewuser.solutions.twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        int[] result = new int[]{-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+                return result;
+            }
+        }
+        return result;
+    }
+}
